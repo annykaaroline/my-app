c2d6f383ec2b4b55e556d658d0fd2740144fc99e
ANNY KAROLINE PINHEIRO ZAPAROLI <anny.zaparoli>
2024-03-01 11:42:37 -0300

    reset css e link

---
+++ src/app/sobre/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+export default function Sobre () {
+    return(
+     <>
+        <h1>Sobre</h1>
+        <p>Tela sobre o meu projeto</p>
+        <Link href={"/"}>Home</Link>
+     </>
+    );
+}
